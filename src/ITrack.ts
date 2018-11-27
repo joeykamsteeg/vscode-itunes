1f6e0ee7f627d024a4b81a3ca2475b8822ec1d87
Joey Kamsteeg <joey_kamsteeg@live.nl>
2018-11-27 22:11:04 +0100

    fixed double quotes in title

--- src/ITrack.ts
+++ src/ITrack.ts
@@ -1,13 +1,13 @@
 export declare type MediaType = "alert tone" | "audiobook" | "book" | "‌home video" | "iTunesU" | "movie" | "song" | "music video" | "podcast" | "ringtone" | "TV show" | "voice memo" | "unknown";
 
 export default interface ITrack {
-    artist: string;
-    name: string;
-    state: string;
-    repeat: string;
-    muted: boolean;
-    volume: number;
-    shuffle: any;
-    album: string;
-    kind: MediaType;
+    artist?: string;
+    name?: string;
+    state?: string;
+    repeat_song?: string;
+    muted?: boolean;
+    volume?: number;
+    shuffle?: any;
+    album?: string;
+    kind?: MediaType;
 }
